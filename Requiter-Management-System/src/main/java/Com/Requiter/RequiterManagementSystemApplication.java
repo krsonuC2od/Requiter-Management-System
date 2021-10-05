f3c9c858320025b2b123fbb705c57753272fbc7b
krsonuC2od <190101120062@cutm.ac.in>
2021-10-05 15:03:53 +0530

    Start

---
+++ Requiter-Management-System/src/main/java/Com/Requiter/RequiterManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package Com.Requiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequiterManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequiterManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Requiter-Management-System/src/test/java/Com/Requiter/RequiterManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.Requiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequiterManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
